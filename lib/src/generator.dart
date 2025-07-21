d41d60edbb4277a3897b3ce244ff6bee969e0ed8
Niclas Prock <niclas.prock@elmatrix.de>
2025-07-21 17:28:01 +0200

    FEATURE: forward object method overrides in annotation

--- lib/src/generator.dart
+++ lib/src/generator.dart
@@ -23,8 +23,61 @@ class DecoratorGenerator extends GeneratorForAnnotation<Decorator> {
       );
     }
 
+    // Extract forwarding settings from annotation and merge with build.yaml settings
+    final annotationForwardingSettings = _extractForwardingSettings(annotation);
+    final mergedSettings = _mergeForwardingSettings(
+      buildYamlSettings: methodNameToIsForwarding,
+      annotationSettings: annotationForwardingSettings,
+    );
+
     // Create decorator builder and generate the code
-    final builder = DecoratorBuilder(element, methodNameToIsForwarding);
+    final builder = DecoratorBuilder(element, mergedSettings);
     return builder();
+  }
+
+  /// Extracts forwarding settings from the @Decorator annotation
+  Map<String, bool> _extractForwardingSettings(ConstantReader annotation) {
+    final forwardingSettings = <String, bool>{};
+
+    // Read each forwarding property from the annotation
+    final forwardToString = annotation.read('forwardToString');
+    if (!forwardToString.isNull) {
+      forwardingSettings['toString'] = forwardToString.boolValue;
+    }
+
+    final forwardEquals = annotation.read('forwardEquals');
+    if (!forwardEquals.isNull) {
+      forwardingSettings['=='] = forwardEquals.boolValue;
+    }
+
+    final forwardHashCode = annotation.read('forwardHashCode');
+    if (!forwardHashCode.isNull) {
+      forwardingSettings['hashCode'] = forwardHashCode.boolValue;
+    }
+
+    final forwardRuntimeType = annotation.read('forwardRuntimeType');
+    if (!forwardRuntimeType.isNull) {
+      forwardingSettings['runtimeType'] = forwardRuntimeType.boolValue;
+    }
+
+    final forwardNoSuchMethod = annotation.read('forwardNoSuchMethod');
+    if (!forwardNoSuchMethod.isNull) {
+      forwardingSettings['noSuchMethod'] = forwardNoSuchMethod.boolValue;
+    }
+
+    return forwardingSettings;
+  }
+
+  /// Merges forwarding settings with annotation settings taking precedence over build.yaml settings
+  Map<String, bool> _mergeForwardingSettings({
+    required Map<String, bool> buildYamlSettings,
+    required Map<String, bool> annotationSettings,
+  }) {
+    final merged = Map<String, bool>.from(buildYamlSettings);
+
+    // Annotation settings override build.yaml settings
+    merged.addAll(annotationSettings);
+
+    return merged;
   }
 }
---
+++ test/edge_cases/annotation_forwarding_test_library.dart
@@ -0,0 +1,152 @@
+import 'package:decorator_annotation/decorator_annotation.dart';
+import 'package:source_gen_test/annotations.dart';
+
+// Class that overrides annotation settings to forward toString but not equals
+@ShouldGenerate(r'''
+class ForwardToStringOnlyDecorator implements ForwardToStringOnly {
+  final ForwardToStringOnly forwardToStringOnly;
+
+  ForwardToStringOnlyDecorator({required this.forwardToStringOnly});
+
+  @override
+  String toString() {
+    return forwardToStringOnly.toString();
+  }
+}
+''')
+@Decorator(forwardToString: true, forwardEquals: false)
+class ForwardToStringOnly {
+  @override
+  String toString() => 'ForwardToStringOnly';
+
+  @override
+  bool operator ==(Object other) => other is ForwardToStringOnly;
+}
+
+// Class that forwards all Object methods using annotation
+@ShouldGenerate(r'''
+class ForwardAllObjectMethodsDecorator implements ForwardAllObjectMethods {
+  final ForwardAllObjectMethods forwardAllObjectMethods;
+
+  ForwardAllObjectMethodsDecorator({required this.forwardAllObjectMethods});
+
+  @override
+  String toString() {
+    return forwardAllObjectMethods.toString();
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return forwardAllObjectMethods == other;
+  }
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) {
+    return forwardAllObjectMethods.noSuchMethod(invocation);
+  }
+
+  @override
+  int get hashCode => forwardAllObjectMethods.hashCode;
+
+  @override
+  Type get runtimeType => forwardAllObjectMethods.runtimeType;
+}
+''')
+@Decorator(
+  forwardToString: true,
+  forwardEquals: true,
+  forwardHashCode: true,
+  forwardRuntimeType: true,
+  forwardNoSuchMethod: true,
+)
+class ForwardAllObjectMethods {
+  @override
+  String toString() => 'ForwardAllObjectMethods';
+
+  @override
+  bool operator ==(Object other) => other is ForwardAllObjectMethods;
+
+  @override
+  int get hashCode => 42;
+
+  @override
+  Type get runtimeType => super.runtimeType;
+
+  @override
+  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
+
+// Class that explicitly disables all forwarding through annotation
+@ShouldGenerate(r'''
+class ForwardNoneDecorator implements ForwardNone {
+  final ForwardNone forwardNone;
+
+  ForwardNoneDecorator({required this.forwardNone});
+}
+''')
+@Decorator(
+  forwardToString: false,
+  forwardEquals: false,
+  forwardHashCode: false,
+  forwardRuntimeType: false,
+  forwardNoSuchMethod: false,
+)
+class ForwardNone {
+  @override
+  String toString() => 'ForwardNone';
+
+  @override
+  bool operator ==(Object other) => other is ForwardNone;
+
+  @override
+  int get hashCode => 42;
+
+  @override
+  Type get runtimeType => super.runtimeType;
+
+  @override
+  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
+
+// Class that only uses some annotation parameters (mixed with defaults)
+@ShouldGenerate(r'''
+class PartialForwardingDecorator implements PartialForwarding {
+  final PartialForwarding partialForwarding;
+
+  PartialForwardingDecorator({required this.partialForwarding});
+
+  @override
+  String toString() {
+    return partialForwarding.toString();
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return partialForwarding == other;
+  }
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) {
+    return partialForwarding.noSuchMethod(invocation);
+  }
+
+  @override
+  int get hashCode => partialForwarding.hashCode;
+}
+''')
+@Decorator(
+  forwardNoSuchMethod: true,
+) // Only override noSuchMethod, others use defaults
+class PartialForwarding {
+  @override
+  String toString() => 'PartialForwarding';
+
+  @override
+  bool operator ==(Object other) => other is PartialForwarding;
+
+  @override
+  int get hashCode => 42;
+
+  @override
+  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
